5b926adfe0774ba4a257edb6e398731bb3309ff1
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2023-02-17 23:25:03 +0200

    working with JSON files

---
+++ LeafletForBlazor NuGet and JSON file/LeafletForBlazor NuGet and JSON file/Program.cs
@@ -0,0 +1,11 @@
+using LeafletForBlazor_NuGet_and_JSON_file;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
