7979f362d5c75a76edc8ffc184c3b520c8552618
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2023-03-11 15:50:42 +0200

    Esri Leaflet plugins Geocoding Search
    Working with Geocoding Search Plugin

---
+++ Esri Leaflet plugins Geocoding Search/Esri Leaflet plugins Geocoding Search/Program.cs
@@ -0,0 +1,11 @@
+using Esri_Leaflet_plugins_Geocoding_Search;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
