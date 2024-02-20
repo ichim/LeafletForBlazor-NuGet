11a2185f3f7af0d614f0de5188bc0947f3edb8dc
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2024-02-20 14:52:07 +0200

    Esri is the leading developer of GIS software
    Esri is the leading developer of GIS software

---
+++ RTM AppearanceOnType/RTM AppearanceOnType/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RTM_AppearanceOnType;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
