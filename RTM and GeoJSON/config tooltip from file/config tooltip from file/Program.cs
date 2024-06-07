f90125c81041bee512be22da21c31184982c2f70
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2024-06-07 12:20:08 +0300

    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping.
    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping.
    The Science of Where!

---
+++ RTM and GeoJSON/config tooltip from file/config tooltip from file/Program.cs
@@ -0,0 +1,11 @@
+using config_tooltip_from_file;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
