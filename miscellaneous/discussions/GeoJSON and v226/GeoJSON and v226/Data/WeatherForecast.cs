47540a3667080dfddb8abc2f7a9be270e76e2ef9
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2025-07-04 08:04:10 +0300

    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping. The Science of Where!
    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping.
    The Science of Where!

---
+++ miscellaneous/discussions/GeoJSON and v226/GeoJSON and v226/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace GeoJSON_and_v226.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
