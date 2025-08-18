631a18ec2881622836fab5aa3b0de0d79f3870b0
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2025-08-18 08:24:43 +0300

    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping. The Science of Where!
    Esri is the global market leader in geographic information system (GIS) software, location intelligence, and mapping.
    The Science of Where!

---
+++ Working with StreamPoint/Clustering/clickEvent/clickEvent/Data/streamPoints.cs
@@ -0,0 +1,225 @@
+﻿using LeafletForBlazor;
+
+namespace clusters.Data
+{
+    public class DataSource
+    {
+        public List<RealTimeMap.StreamPoint> pointsStart = new List<RealTimeMap.StreamPoint> {
+          new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.45028925827577,
+            longitude =  26.10629452317551
+        },
+     new RealTimeMap.StreamPoint()
+    {
+        latitude = 44.4502578,
+        longitude = 26.1108199,
+        type = "A",
+        value = "type 1",
+guid = Guid.NewGuid()
+    },
+        new RealTimeMap.StreamPoint()
+    {
+        latitude = 44.4500215,
+        longitude = 26.1105407,
+        type = "A",
+        value = "type 2",
+guid = Guid.NewGuid()
+    },
+           new RealTimeMap.StreamPoint()
+    {
+        latitude =44.4497369,
+        longitude = 26.1093086,
+        type = "A",
+        value = "type 3",
+guid = Guid.NewGuid()
+    },
+          new RealTimeMap.StreamPoint()
+    {
+        latitude =44.4496145,
+        longitude = 26.1088460,
+        type = "B",
+        value = "type 4",
+guid = Guid.NewGuid()
+    },
+              new RealTimeMap.StreamPoint()
+    {
+        latitude =44.4491875,
+        longitude = 26.1079328,
+        type = "B",
+        value = "type 5",
+guid = Guid.NewGuid()
+    }, };
+        public List<RealTimeMap.StreamPoint> pointsMore = new List<RealTimeMap.StreamPoint>
+    {
+        new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.45028925827577,
+            longitude =  26.10629452317551
+        },
+         new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.45028925978743,
+            longitude =  26.1066700356264
+        },
+         new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.450151397480745,
+            longitude =  26.107630272512512
+        },
+          new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.449971408657866,
+            longitude =  26.108129162873677
+        },
+                  new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.449783759630726,
+            longitude =  26.108558312961982
+        },
+
+         new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.Parse("18466d7f-0689-4b8e-a2ee-28e5cb27f86e"),
+            type = "A",
+            value = "A",
+            latitude = 44.449677297275045,
+            longitude =  26.108979815254525
+        },
+                   new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.Parse("28466d7f-0689-4b8e-a2ee-28e5cb28f86f"),
+            type = "A",
+            value = "A",
+            latitude = 44.44972603654415,
+            longitude =  26.109226375283605
+        },
+                   new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.Parse("48466d7f-0689-4b8e-a2ee-28e5cb28f86a"),
+            type = "A",
+            value = "A",
+            latitude = 44.44966104701526,
+            longitude =  26.109351550356433
+        },
+                   new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.Parse("48466d7f-0689-4b8e-a2ee-28e5cb28f46f"),
+            type = "A",
+            value = "A",
+            latitude = 44.449487742665156,
+            longitude =  26.10954120744587
+        },
+
+                     new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.44908427257026,
+            longitude =  26.10993190777844
+        },
+                        new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "A",
+            value = "A",
+            latitude = 44.44861851312573,
+            longitude =  26.110417425725984
+        },
+
+         new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.448383012000974,
+            longitude =  26.110687883059306
+        },
+        new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.44823955644303,
+            longitude =  26.11096219742611
+        },
+     new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.451786225805286, 
+            longitude =  26.1070480864821
+        },
+   new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.45248123918695, 
+            longitude =  26.1092154805802
+        },
+ new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.45245580905582,
+            longitude =   26.11153474714201
+        },
+new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.45241363924153, 
+            longitude =   26.105304728224652
+        },
+new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "B",
+            value = "B",
+            latitude = 44.451755436934945, 
+            longitude =   26.10579790791407
+        },
+
+new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "C",
+            value = "C",
+            latitude = 44.447760874141856,
+            longitude =   26.103765798592114
+        },
+new RealTimeMap.StreamPoint()
+        {
+            guid = Guid.NewGuid(),
+            type = "C",
+            value = "C",
+            latitude = 44.44798671636687, 
+            longitude =   26.106584873050217
+        },
+    };
+
+    }
+}
