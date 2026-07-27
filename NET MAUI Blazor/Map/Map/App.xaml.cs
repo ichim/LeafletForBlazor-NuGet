31891164cfdbd115ac3eed9b594b28fe8cb10787
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2026-07-27 21:39:11 +0300

    .NET MAUI Blazor Hybrid App

---
+++ NET MAUI Blazor/Map/Map/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Map
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Map" };
+        }
+    }
+}
---
+++ NET MAUI Blazor/Map/Map/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Map
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
