a874622db6f40889d3b7d9f39618cd2b3b9e3fc8
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2026-07-29 11:31:11 +0300

    .NET MAUI Blazor Hybrid App
    .NET MAUI Blazor Hybrid App

---
+++ MAUI Blazor/StreamPoint events/StreamPoint events/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StreamPoint_events
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "StreamPoint events" };
+        }
+    }
+}
---
+++ MAUI Blazor/StreamPoint events/StreamPoint events/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StreamPoint_events
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
