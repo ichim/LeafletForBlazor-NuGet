be13cea256d884ff129a318bffcee99efa2aa22f
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2026-09-01 18:55:13 +0300

    .NET MAUI Blazor Hybrid App
    .NET MAUI Blazor Hybrid App

---
+++ MAUI Blazor/Components/Legend/Legend/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Legend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Legend" };
+        }
+    }
+}
---
+++ MAUI Blazor/Components/Legend/Legend/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Legend
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
